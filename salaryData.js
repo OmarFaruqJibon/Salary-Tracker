4687d7ce7c0c78e8f97659b18a12b04eb3918af8
unknown <jibon602947@gmail.com>
2022-03-08 22:12:21 +0600

    first commit

---
+++ salaryData.js
@@ -0,0 +1,64 @@
+let salary_data = {
+  0: {
+    hobby: "talking javascript",
+    name: "Kane will",
+    salary: "76340.0",
+  },
+  1: {
+    name: "Danis Bill",
+    salary: "94320.0",
+    status: "married",
+  },
+  2: {
+    name: "Right hut",
+    salary: "67000.0",
+  },
+  3: {
+    name: "Harry",
+    salary: "105540.0",
+  },
+  4: {
+    name: "Shown",
+    salary: "82220.0",
+  },
+  5: {
+    name: "Donald",
+    salary: "64430.0",
+    sleepHour: 4,
+  },
+  6: {
+    favLibrary: "React",
+    name: "Ragnar",
+    pizzaSlice: 5,
+    salary: "36000.0",
+  },
+  7: {
+    name: "Bjon",
+    salary: "44432",
+  },
+  "-JvacZOcfUEvub7mxeo-": {
+    name: "Jibon",
+    organization: "Nielsen",
+    salary: "53454",
+  },
+  "-Jvac_s61TL9oYM5SjMR": {
+    name: "Jibon",
+    salary: "43454",
+  },
+  "-JvacbMTxlHCG5VaVDWg": {
+    name: "Lagatha",
+    salary: "33454",
+  },
+  "-JviNyklLgZqk4rSB8pA": {
+    name: "Iver",
+    salary: "23444",
+  },
+  "-JviO0Y_tNNMQNrdZVBR": {
+    name: "Athestan",
+    salary: "63444",
+  },
+  "-JviO3gXUpSdnZHtliWg": {
+    name: "Floki",
+    salary: "53655",
+  },
+};
